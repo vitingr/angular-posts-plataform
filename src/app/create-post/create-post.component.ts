30af645681291de74c306e3adfba6729eb50607b
Vitor Gabriel <vitorgabrielsbo1460@gmail.com>
2023-11-22 16:56:07 -0300

    Update create-post.component.ts

--- src/app/create-post/create-post.component.ts
+++ src/app/create-post/create-post.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CommentFormComponent } from '../components/comment-form/comment-form.component';
+import { CommentService } from '../services/comment-service';
+import { UserService } from '../services/user-service';
 
 @Component({
   selector: 'app-create-post',
@@ -11,4 +13,29 @@ import { CommentFormComponent } from '../components/comment-form/comment-form.co
 })
 export class CreatePostComponent {
 
+  commentService = inject(CommentService)
+  userService = inject(UserService)
+
+  comments = signal<any[]>([])
+
+
+  createComment(formValues: { text: string }) {
+    const { text } = formValues
+    const user = this.userService.getUserFromStorage()
+
+    if (!user) {
+      return
+    } else {
+      this.commentService.createComment({
+        ownerId: user.id,
+        ownerName: user.name,
+        text
+      }).subscribe(createdComment => {
+        this.comments.set([
+          createdComment,
+          ...this.comments()
+        ])
+      })
+    }
+  }
 }
